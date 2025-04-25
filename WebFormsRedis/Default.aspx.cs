8b01d373f8742d5b8506d091ac64bf9f8031b8e8
Md. Safiul Alam <info.safi570@yahoo.com>
2025-04-25 23:03:21 +0600

    intial push

---
+++ WebFormsRedis/Default.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebFormsRedis
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnSetSession_Click(object sender, EventArgs e)
+        {
+            Session["settime"] = System.DateTime.Now;
+        }
+
+        protected void BtnGetSession_Click(object sender, EventArgs e)
+        {
+            if (Session["settime"] != null)
+            {
+                this.ShowSessionData.Text = Session["settime"].ToString();
+
+            }
+        }
+
+        protected void BtnRemoveSession_Click(object sender, EventArgs e)
+        {
+            Session.Remove("settime");
+            this.ShowSessionData.Text =String.Empty;
+
+        }
+    }
+}
